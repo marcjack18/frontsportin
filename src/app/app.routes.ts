01f30a22e1cca1312c835ee50f1376d68d6154b9
Iván <ivanx.afonso@gmail.com>
2026-01-27 19:20:13 +0100

    finalizando componente usuario-plist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
-import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { UsuarioPlist } from './component/usuario/usuario-plist/usuario-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'usuario/plist', component: UsuarioPlist },
 ];
