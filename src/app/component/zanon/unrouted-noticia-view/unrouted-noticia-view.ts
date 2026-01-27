58d740f0e6e2a73e149c12c5f841b83d40c5af01
DanielZR05 <danielzanon2005@gmail.com>
2026-01-27 10:40:56 +0100

    He creado un componente nuevo

---
+++ src/app/component/zanon/unrouted-noticia-view/unrouted-noticia-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unrouted-noticia-view',
+  imports: [],
+  templateUrl: './unrouted-noticia-view.html',
+  styleUrl: './unrouted-noticia-view.css',
+})
+export class UnroutedNoticiaView {
+
+}
