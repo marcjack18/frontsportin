07f82225f1072ac8a21569a2a7699be3156baeaa
ian <ianechu181005@gmail.com>
2026-01-29 12:43:25 +0100

    Add jugador component with routing, service, and styles; implement search and pagination features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { JugadorPlisComponent } from './component/jugador/jugadorPlist/jugador-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted},
+    { path: 'jugador', component: JugadorPlisComponent},
+    { path: 'jugador/usuario/:id', component: JugadorPlisComponent},
+    { path: 'jugador/equipo/:id', component: JugadorPlisComponent}
 ];
